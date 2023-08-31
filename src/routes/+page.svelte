7f47279629a84a8eb29c044157c99dfeec21861d
Justin Garner <charlottewebsolutions1@gmail.com>
2023-08-31 16:22:05 -0400

    fixed placeholder text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -159,7 +159,7 @@
                 <div class="mb-6">
                     <label for="additionalDetails" class="block text-sm font-medium leading-6 text-gray-900">Additional Details (Optional)</label>
                     <div class="relative mt-2 rounded-md shadow-sm">
-                        <input type="text" name="additionalDetails" id="additionalDetails" class="block w-full rounded-md border-0 pt-3 pb-16 pl-4 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:leading-6" placeholder="My dog died" bind:value={additionalDetails}>
+                        <input type="text" name="additionalDetails" id="additionalDetails" class="block w-full rounded-md border-0 pt-3 pb-16 pl-4 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:leading-6" placeholder="" bind:value={additionalDetails}>
                     </div>
                 </div>
 
